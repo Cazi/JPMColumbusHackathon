cb65f690241c7d7faeaaccb0ed9b20e35e931b46
Aaron Amankwaah <amankwaah123@gmail.com>
2022-07-15 10:51:37 -0400

    testing #2

--- one-step/src/components/pages/HomePage/HomePage.tsx
+++ one-step/src/components/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePageContainer = styled.div`
   background-color: green;
 `;
 const HomePage: FC = () => {
-  return <HomePageContainer>Home Page, Aaron push test</HomePageContainer>;
+  return <HomePageContainer>Home Page, Aaron push test branching</HomePageContainer>;
 };
 
 export default HomePage;
