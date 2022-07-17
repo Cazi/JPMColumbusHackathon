f1b4f416b30b4fab7d066745c20a377cc44de668
Aaron Amankwaah <amankwaah123@gmail.com>
2022-07-16 23:15:26 -0400

    homepage api attempt

--- one-step/src/components/pages/HomePage/HomePage.tsx
+++ one-step/src/components/pages/HomePage/HomePage.tsx
@@ -1,5 +1,33 @@
 import { FC } from "react";
 import styled from "styled-components";
+// import axios from "axios";
+
+// API chunk 
+import React, { useEffect, useState } from 'react';
+import Articles from './Articles.js';
+import axios from 'axios';
+
+// added chunk into home page render/return
+// const App = () => {
+//   const [loading, setLoading] = useState(false);
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     const getArticles = async () => {
+//       setLoading(true);
+//       const res = await axios.get(` https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("Arts")&sort=newest&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+//       setArticles(res.data.response.docs);
+
+//       setLoading(false);
+//     };
+//     getArticles();
+
+//   }, []);
+
+
+// }
+
+
 
 // const TitleContainer = styled.div`
 //   width: 100%;
@@ -48,7 +76,7 @@ const ColumnsContainer = styled.div`
 `;
 
 // each news blurb contianer
-const NewsContainer= styled.div`
+const NewsContainer = styled.div`
   display: inline-flex;
   flex-direction: column-reverse;
 `;
@@ -64,39 +92,62 @@ const NewsBlurbs = styled.div`
   // padding: 10px:
 `;
 
+const HomePage: FC
+  //  = () => {
+  = () => {
+    const [loading, setLoading] = useState(false);
+    const [articles, setArticles] = useState([]);
 
-const HomePage: FC = () => {
-  return <HomePageContainer>
-    <BodyContainer >
-      {/* API NEWS GOES HERE */}
-      <ColumnsContainer>
-        <TitleContainer>
-          <StyledSubTitle>
-            News
-            <hr/>
-          </StyledSubTitle>
-        </TitleContainer>
-        <NewsContainer>
-          {/* for loop for it automatically push 10 news from NYT here */}
-          <NewsBlurbs>
-            <a href='https://www.jpmorganchase.com/'> <h3>News Title</h3></a>
-            <h4>Author: Thomas Edison</h4>
-            <body>
-              <p>JPMC is Awesome</p>
-            </body>
-            <hr />
-          </NewsBlurbs>
+    useEffect(() => {
+      const getArticles = async () => {
+        setLoading(true);
+        const res = await axios.get(` https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("Arts")&sort=newest&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+        setArticles(res.data.response.docs);
 
-          <NewsBlurbs>
-            <a href='https://www.jpmorganchase.com/'> <h3> Title News</h3></a>
-            <h4>Author: Bob TOm Edison</h4>
-            <body>
-              <p>I am is Awesome</p>
-            </body>
-            <hr />
-          </NewsBlurbs>
-        </NewsContainer>
-        {/* {% for announcement in updatesData%}
+        setLoading(false);
+      };
+      getArticles();
+
+    }, []);
+    return <HomePageContainer>
+      <BodyContainer >
+        {/* API NEWS GOES HERE */}
+        <ColumnsContainer>
+          <TitleContainer>
+            <StyledSubTitle>
+              News
+              <hr />
+            </StyledSubTitle>
+          </TitleContainer>
+          <NewsContainer>
+            {/* for loop for it automatically push 10 news from NYT here */}
+            <NewsBlurbs>
+              <a href='https://www.jpmorganchase.com/'> <h3>News Title</h3></a>
+              <h4>Author: Thomas Edison</h4>
+              <body>
+                <p>JPMC is Awesome</p>
+              </body>
+              <hr />
+            </NewsBlurbs>
+
+            <NewsBlurbs>
+              <a href='https://www.jpmorganchase.com/'> <h3> Title News</h3></a>
+              <h4>Author: Bob TOm Edison</h4>
+              <body>
+                <p>I am is Awesome</p>
+              </body>
+              <hr />
+            </NewsBlurbs>
+
+            <NewsBlurbs>
+              <div>
+                {/* found and a potential answer but don't know how to intepret it 
+                https://stackoverflow.com/questions/42657792/typescript-react-redux-property-xxx-does-not-exist-on-type-intrinsicattrib */}
+                {/* <Articles loading={loading} articles={articles} /> */}
+              </div >
+            </NewsBlurbs>
+          </NewsContainer>
+          {/* {% for announcement in updatesData%}
         <div class ="row ">
             <div class ="col-12 ">
         <h1> <b> {{ announcement.update_heading }}</b> by {{ announcement.update_messenger }} </h1> 
@@ -106,24 +157,22 @@ const HomePage: FC = () => {
             </div>
         </div><hr>
         {% endfor %} */}
-        {/* News titile, author, date, import summary/snapshot, 
+          {/* News titile, author, date, import summary/snapshot, 
          */}
-
-
-      </ColumnsContainer>
-      {/* Right Column  */}
-      <ColumnsContainer>
-        <TitleContainer>
-          <StyledSubTitle>
-            Random Facts
-          </StyledSubTitle>
-        </TitleContainer>
-        <body>
-          <p>Some text..</p>
-        </body>
-      </ColumnsContainer>
-    </BodyContainer>
-  </HomePageContainer>;
-};
+        </ColumnsContainer>
+        {/* Right Column  */}
+        <ColumnsContainer>
+          <TitleContainer>
+            <StyledSubTitle>
+              Random Facts
+            </StyledSubTitle>
+          </TitleContainer>
+          <body>
+            <p>Some text..</p>
+          </body>
+        </ColumnsContainer>
+      </BodyContainer>
+    </HomePageContainer>;
+  };
 
 export default HomePage;
