9d832c039b52da7cdbe12a66b2e634746c846820
Ayo Obisesan <77123546+Ayo-SE@users.noreply.github.com>
2022-07-20 14:55:36 -0400

    Update MyStepsPage.tsx

--- one-step/src/components/pages/MyStepsPage/MyStepsPage.tsx
+++ one-step/src/components/pages/MyStepsPage/MyStepsPage.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const MyStepsPageContainer = styled.div`
+    height: 100vh;
+    background-color: #F0EBE3;
 `
 
 const MyStepsPage: FC = () => {
