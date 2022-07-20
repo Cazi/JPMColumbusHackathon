c77be2eddb02441002f1f8b058c38c44f4df4d83
Ayo Obisesan <77123546+Ayo-SE@users.noreply.github.com>
2022-07-20 16:15:19 -0400

    MyStepsPage Structure Changes

--- one-step/src/components/pages/MyStepsPage/MyStepsPage.tsx
+++ one-step/src/components/pages/MyStepsPage/MyStepsPage.tsx
@@ -8,10 +8,49 @@ const MyStepsPageContainer = styled.div`
     background-color: #F0EBE3;
 `
 
+const ChartContainer = styled.div`
+    padding-top: 20px;
+    padding-left: 20px;
+    height: 63%;
+`
+
+const TitleContainer = styled.div`
+    padding: 20px;
+`
+
+const BoxedContainer = styled.div`
+    background-color: #F0EBE3;
+    padding-top: 200px;
+    display: flex;
+    align-items: row;
+    justify-content: space-around;
+    padding-bottom: 100px;
+`
+
+const CalculatorBox = styled.div`
+    height: 500px;
+    width: 600px;
+    background-color: gray;
+`
+
+const NewsBox = styled.div`
+    height: 500px;
+    width: 600px;
+    background-color: gray;
+`
+
 const MyStepsPage: FC = () => {
     return(
         <MyStepsPageContainer>
-            <LoanPaydownChart data={myData} />
+            <TitleContainer><h1>Welcome to One Step</h1></TitleContainer>
+            <ChartContainer>
+                <h2>My Steps: </h2>
+               <LoanPaydownChart data={myData} /> 
+            </ChartContainer>
+            <BoxedContainer>
+                <CalculatorBox>s</CalculatorBox>
+                <NewsBox>f</NewsBox>
+            </BoxedContainer>
         </MyStepsPageContainer>
     )
 }
