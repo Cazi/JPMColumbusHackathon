f9b27e780c12f57fe751a7070a579e2b1af8f7f3
myra-anigbo <myra.anigbo@gmail.com>
2022-07-21 10:17:09 -0400

    updating learn more

--- one-step/src/components/pages/LearnMorePage/LearnMorePage.tsx
+++ one-step/src/components/pages/LearnMorePage/LearnMorePage.tsx
@@ -93,8 +93,8 @@ const LearnMorePage: FC = () => {
           <p><b>Problem: </b>For 44 million Americans, stress is created by the burden of a combined
             1.7 trillion dollars in student loan debt. Education is suppose to improve quality of life, but many feel hopeless when it comes to the expenses of higher education. Many lack the resources,
             knowledge, and planning needed to alleviate student debt.</p>
-          <p><b>Solution: </b>Many don’t know where to start when it comes to tackling their student loans.
-            That’s where One Step comes in. One Step is a website to help navigate those who need help with handling their student loans.
+          <p><b>Solution: </b>Many don’t know where to start when it comes to tackling student loans.
+            That’s where One Step comes in. One Step is a website to help those who need to naviagate student loans practically.
             Our web page includes a loan calculator, news, and forgiveness resources to aid in student loan journeys.  </p>
           <p><b>Future Plans: </b>Our hope is to empower people to have the confidence to pay off their loans.
             Everyone should pursue a higher education without having to be burdened by student debt. We want to increase
