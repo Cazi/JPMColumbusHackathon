c41c23a71eb24524505b5b69d12e990862554c7c
Ayo Obisesan <77123546+Ayo-SE@users.noreply.github.com>
2022-07-21 12:36:27 -0400

    LearnMorePage Styling

--- one-step/src/components/pages/LearnMorePage/LearnMorePage.tsx
+++ one-step/src/components/pages/LearnMorePage/LearnMorePage.tsx
@@ -54,6 +54,7 @@ const AboutMe = styled.div`
 `
 
 const MyFace = styled.img`
+  border: 3px solid #324B4E;
   height: 200px;
   width: 200px;
 `
@@ -72,7 +73,7 @@ const WhatWeDoContainer = styled.div`
   background-color: #e4dccf;
   padding: 20px;
   margin: 10px;
-  width: 60%;
+  width: 70%;
   color: #324b4e;
 `;
 const LearnMoreContainer = styled.p`
@@ -84,6 +85,7 @@ const MissionStatementContainer = styled.div`
   justify-content: center;
   padding: 26px;
   margin: 20px;
+  width: 70%;
   font-size: 24px;
   border: 3px solid #324b4e;
   border-radius: 15px;
@@ -102,6 +104,7 @@ const StyledFooter = styled.footer`
   margin: 20px;
   font-size: 17px;
   font-weight: bold;
+  width: 70%;
   color: #324b4e;
   border: 3px solid #324b4e;
   border-radius: 15px;
@@ -114,33 +117,16 @@ const LearnMorePage: FC = () => {
         <HeaderStyled2>
           What is One Step?
         </HeaderStyled2>
-      <MissionStatementContainer>
-        <MissionStatement>
-          <strong>One Step</strong> was born out of a drive to help curb the overwhelming nature of student loans. Marginalized communities have a particularly difficult time navigating
-          and decifering the world of student loans, and so we created One Step to help guide our people towards becoming <strong>debt free</strong> one step at a time!
-        </MissionStatement>
-      </MissionStatementContainer>
-
-      <HeaderStyled>
-        What We Do
-      </HeaderStyled>
-      <CenterDisplay>
-        <WhatWeDoContainer>
-          <p><b>Problem: </b>For 44 million Americans, stress is created by the burden of a combined
-            1.7 trillion dollars in student loan debt. Education has the capacity to improve quality of life, but many feel hopeless when it comes to the expenses of higher education due to lack of resources,
-            knowledge, and planning.</p>
-          <p><b>Solution: </b>Many don’t know where to start when it comes to tackling student loans.
-            That’s where One Step comes in. One Step is a website designed to help manage student loans effectively.
-            Our web page includes a loan calculator, news, and forgiveness resources to aid in student loan journeys.  </p>
-          <p><b>Future Plans: </b>Our hope is to empower people by creating an effecient process to manage student loans.
-            Everyone should have the ability to pursue a higher education without the burden of student debt. We want to raise
-            awareness of loan management tools the public may not be aware of. We will help you on your journey, one step at a time.</p>
-        </WhatWeDoContainer>
-      </CenterDisplay>
-      <br />
+        <CenterDisplay>
+          <MissionStatementContainer>
+            <MissionStatement>
+              <strong>One Step</strong> was born out of a drive to help curb the overwhelming nature of student loans. Marginalized communities have a particularly difficult time navigating
+              and decifering the world of student loans, and so we created One Step to help guide our people towards becoming <strong>debt free</strong> one step at a time!
+            </MissionStatement>
+          </MissionStatementContainer>
+        </CenterDisplay>
       
-      <br />
-      <br />
+
       <HeaderStyled>
         Who We Are
       </HeaderStyled>
@@ -196,21 +182,44 @@ const LearnMorePage: FC = () => {
         </ColumnsDivs>
       </ContentStyled>
       <br />
+      
+      <br />
+      <br />
+      <HeaderStyled>
+        What We Do
+      </HeaderStyled>
+      <CenterDisplay>
+        <WhatWeDoContainer>
+          <p><b>Problem: </b>For 44 million Americans, stress is created by the burden of a combined
+            1.7 trillion dollars in student loan debt. Education has the capacity to improve quality of life, but many feel hopeless when it comes to the expenses of higher education due to lack of resources,
+            knowledge, and planning.</p>
+          <p><b>Solution: </b>Many don’t know where to start when it comes to tackling student loans.
+            That’s where One Step comes in. One Step is a website designed to help manage student loans effectively.
+            Our web page includes a loan calculator, news, and forgiveness resources to aid in student loan journeys.  </p>
+          <p><b>Future Plans: </b>Our hope is to empower people by creating an effecient process to manage student loans.
+            Everyone should have the ability to pursue a higher education without the burden of student debt. We want to raise
+            awareness of loan management tools the public may not be aware of. We will help you on your journey, one step at a time.</p>
+        </WhatWeDoContainer>
+      </CenterDisplay>
+      <br />
 
 
       <HeaderStyled>
         Acknowledgements and Appreciation
       </HeaderStyled>
-      <StyledFooter>
-        We want to give a huge thank you to the following people for guiding us in our learning process during this hackathon.
-        As mentors, you’ve spent invaluable time and energy helping us become better programmers, teammembers, and students.
-        We’ve gained so much experience, and it’s all thanks to you all.
-        <br />
-        <br />
-        Ahmed Adbulahi, Alyssa J.C. Alleyne-Atherly, Amie C. Jagne, Eric Chang, Gail Mitchell-Cousar, Harry Colson, Jermontae Ponder
-        Kendallene Hill, Malcolm D. Player, Matthew E. Baker, Matthew E. Miller, Sam Florentine, Susanne R. South, Tamia L. Stewart, and
-        Todd Tarney, and the wider ABP/AHL staff.
-      </StyledFooter>
+      <CenterDisplay>
+        <StyledFooter>
+          We want to give a huge thank you to the following people for guiding us in our learning process during this hackathon.
+          As mentors, you’ve spent invaluable time and energy helping us become better programmers, teammembers, and students.
+          We’ve gained so much experience, and it’s all thanks to you all.
+          <br />
+          <br />
+          Ahmed Adbulahi, Alyssa J.C. Alleyne-Atherly, Amie C. Jagne, Eric Chang, Gail Mitchell-Cousar, Harry Colson, Jermontae Ponder
+          Kendallene Hill, Malcolm D. Player, Matthew E. Baker, Matthew E. Miller, Sam Florentine, Susanne R. South, Tamia L. Stewart, and
+          Todd Tarney, and the wider ABP/AHL staff.
+        </StyledFooter>
+      </CenterDisplay>
+      
       <br />
       <br />
     </LearnMorePageContainer>
