68d08ac3c80b11ff1a57a58bd3582f82fc865a49
Ayo Obisesan <77123546+Ayo-SE@users.noreply.github.com>
2022-07-21 11:21:18 -0400

    Register and Login Page Creation

---
+++ one-step/src/components/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const RegisterPageContainer = styled.div`
+    height: 100vh;
+    background-color: #F0EBE3;
+`
+
+const RegisterPage: FC = () => {
+    return(
+        <RegisterPageContainer>
+            Register
+        </RegisterPageContainer>
+    )
+}
+
+export default RegisterPage;
